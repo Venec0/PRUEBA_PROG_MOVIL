0f0f51f34f9da6f9d7d802dbc95ceccaa003cbf4
Venec0 <joeljesus30030@gmail.com>
2023-11-14 11:48:46 -0300

    RE ESTRUCTURACIÓN DE PÁGINAS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,10 +17,6 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: 'login2',
-    loadChildren: () => import('./login2/login2.module').then( m => m.Login2PageModule)
-  },
-  {
     path: 'signup',
     loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
   },
