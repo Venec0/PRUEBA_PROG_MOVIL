2611f3df891e1b3b2369ac55dbe5ac50f03a94ed
FabianAguila <fabian.andres.aguila@gmail.com>
2023-11-17 01:03:41 -0300

    LOGIN RENOVADO Y FUNCIONAL

--- src/app/servicios/guardado.service.ts
+++ src/app/servicios/guardado.service.ts
@@ -8,39 +8,47 @@ const storageUser = "userData";
   providedIn: 'root'
 })
 export class LogInService {
-  public userMail = "";
+  public userCorreo = "";
+  constructor() { }
 
-  constructor() {}
+    //obtiene un elemento segun su key desde el Storage (capacitor)
 
   async getItem(llave:string):Promise<string | null>{
     const obj = await Preferences.get({key:llave});
     return obj.value;
   }
 
-  async setItem(llave:string, valor:string) {
-    await Preferences.set({key:llave, value:valor});
+  //agrega un valor al storage
+  async setItem(llave:string, valor:string){
+    await Preferences.set({key:llave,value:valor});
   }
 
-  async obtenerUser(){
+//obtiene un usuario desde el storage
+  async obtenerUsuario(){
     const storageData = await this.getItem(storageUser);
-    if (storageData == null){
-      return[];
-    } 
-  
-  const data:any[] = JSON.parse(storageData);
-  if(data) {
-    return data;
-  }else{
-    return[];
+    if (storageData == null) {
+      return [];
+    }
+
+    const data:any[] = JSON.parse(storageData);
+    if (data) {
+      return data;
+    }else{
+      return [];
+    }
   }
-  }
-  async agregarUser(user:any[]){
-    const users = await this.obtenerUser();
-    for(const i of users) {
+// agrega un user al storage
+  async agregarUsuario(user:any[]){
+    const usuarios = await this.obtenerUsuario();
+    for (const i of usuarios) {
       if (i) {
         user.push(i);
       }
     }
-    this.setItem(storageUser, JSON.stringify(user));
+
+    this.setItem(storageUser,JSON.stringify(user));
   }
+
+
+
 }
